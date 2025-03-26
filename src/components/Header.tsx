52e94ff5f03b403489cde4109b7c89b1a22a81d6
JuliaRebelles05 <juliarebelles@gmail.com>
2025-03-26 20:34:11 -0300

    Implementação das Funcionalidades de Agendamento

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,20 @@
 import { StatusBar } from 'react-native';
 import styled from 'styled-components/native';
+import theme from '../styles/themes';
 
 export const HeaderContainer = styled.View`
-  background-color: #007BFF;
+  background-color: ${theme.colors.primary};
   padding-top: ${StatusBar.currentHeight}px;
-  padding: 20px;
+  padding: ${theme.spacing.medium}px;
+  elevation: 4;
+  shadow-color: #000;
+  shadow-opacity: 0.3;
+  shadow-radius: 4px;
+  shadow-offset: 0px 2px;
 `;
 
 export const HeaderTitle = styled.Text`
-  color: white;
-  font-size: 20px;
-  font-weight: bold;
+  color: ${theme.colors.white};
+  font-size: ${theme.typography.title.fontSize}px;
+  font-weight: ${theme.typography.title.fontWeight};
 `;
